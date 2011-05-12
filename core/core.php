9cb3d1b6635e235ef368d2d82e5c70cdb2545f77
ivan <ivan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-12 19:04:16 +0000

    Fix taxonomies sort notice.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/directory@3403 f53fe46e-8c02-459b-9fdd-93e75771b278

--- core/core.php
+++ core/core.php
@@ -186,7 +186,10 @@ class Directory_Core {
      */
     function sort_custom_taxonomies( $sort ) {
         $options = $this->get_options('general_settings');
-        $sort = $options['order_taxonomies'];
+
+		if ( isset( $options['order_taxonomies'] ) )
+			$sort = $options['order_taxonomies'];
+
         return $sort; 
     }
 
@@ -298,13 +301,13 @@ if ( class_exists('Directory_Core') )
 	$__directory_core = new Directory_Core();
 
 /* Update Notifications Notice */
-if ( !function_exists( 'wdp_un_check' ) ):
-function wdp_un_check() {
-    if ( !class_exists('WPMUDEV_Update_Notifications') && current_user_can('edit_users') )
-        echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-}
-add_action( 'admin_notices', 'wdp_un_check', 5 );
-add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-endif;
+// if ( !function_exists( 'wdp_un_check' ) ):
+// function wdp_un_check() {
+    // if ( !class_exists('WPMUDEV_Update_Notifications') && current_user_can('edit_users') )
+        // echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+// }
+// add_action( 'admin_notices', 'wdp_un_check', 5 );
+// add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+// endif;
 
 ?>
