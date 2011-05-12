9d0fe5a6a85c4d32635d24a3c07af89ba137642f
ivan <ivan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-12 19:16:12 +0000

    Remove old Directory theme files
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/directory@3424 f53fe46e-8c02-459b-9fdd-93e75771b278

--- themes/hca/home.php
+++
@@ -1,8 +0,0 @@
-<?php get_header() ?>
-	<?php locate_template( array( '/includes/components/searchcontainer.php' ), true ); ?>
-			<?php locate_template( array( '/includes/components/articleblock.php' ), true ); ?>
-
-			<?php locate_template( array( '/includes/components/directorylist.php' ), true ); ?>
-			
-			<?php locate_template( array( '/includes/components/topresults.php' ), true ); ?>
-<?php get_footer() ?>
--- themes/hca/includes/components/branding-header.php
+++
@@ -1,5 +0,0 @@
-<!-- start branding -->
-<div id="logo">
-	<img src="<?php bloginfo('template_directory'); ?>/images/hcaimages/logo.png" alt="HealthReview"/>
-</div>
-<!-- end branding -->
