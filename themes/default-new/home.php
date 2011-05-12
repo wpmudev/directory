c878ee46f38a337167905524ef2c651c55ca6c73
ivan <ivan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-12 19:06:07 +0000

    Theme testing.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/directory@3413 f53fe46e-8c02-459b-9fdd-93e75771b278

--- themes/default-new/home.php
+++ themes/default-new/home.php
@@ -2,8 +2,9 @@
 <div id="content"><!-- start #content -->
 	<div class="padder">
 		<ul>
-    <?php dp_list_categories('home'); ?>
-</ul>
+			<?php // the_listing_tags(); ?>
+			<?php the_listing_categories(); ?>
+		</ul>
 	</div>
 </div><!-- end #content -->
 <?php get_footer() ?>
