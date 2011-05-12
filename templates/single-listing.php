c408cba189a78700ce4a50974c7589169653f509
ivan <ivan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-12 19:05:59 +0000

    WIP, single Listings template.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/directory@3412 f53fe46e-8c02-459b-9fdd-93e75771b278

--- templates/single-listing.php
+++ templates/single-listing.php
@@ -1,29 +1,31 @@
-
 <?php get_header(); ?>
 
 		<div id="listing-container">
 			<div id="listing-content" role="main">
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 				<div id="listing-nav-above" class="listing-navigation">
 					<div class="listing-nav-previous"><?php previous_post_link( '%link', '<span class="listing-meta-nav">' . _x( '&larr;', 'Previous post link', DP_TEXTDOMAIN ) . '</span> %title' ); ?></div>
 					<div class="listing-nav-next"><?php next_post_link( '%link', '%title <span class="listing-meta-nav">' . _x( '&rarr;', 'Next post link', DP_TEXTDOMAIN ) . '</span>' ); ?></div>
-				</div><!-- #nav-above -->
+				</div><!-- #listing-nav-above -->
 
 				<div id="listing-post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<h1 class="listing-entry-title"><?php the_title(); ?></h1>
 
 					<div class="listing-entry-meta">
-						<?php twentyten_posted_on(); ?>
-					</div><!-- .entry-meta -->
+						<?php // twentyten_posted_on(); ?>
+					</div><!-- .listing-entry-meta -->
 
 					<div class="listing-entry-content">
 						<?php the_content(); ?>
+						<?php the_listing_tags(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', DP_TEXTDOMAIN ), 'after' => '</div>' ) ); ?>
-					</div><!-- .entry-content -->
+					</div><!-- .listing-entry-content -->
 
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+
+					<?php if ( get_the_author_meta( 'description' ) ) : 
+					// If a user has filled out their description, show a bio on their entries  ?>
 
 					<div id="listing-entry-author-info">
 						<div id="listing-author-avatar">
@@ -36,29 +38,29 @@
 								<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
 									<?php printf( __( 'View all posts by %s <span class="listing-meta-nav">&rarr;</span>', DP_TEXTDOMAIN ), get_the_author() ); ?>
 								</a>
-							</div><!-- #author-link	-->
-						</div><!-- #author-description -->
-					</div><!-- #entry-author-info -->
-<?php endif; ?>
+							</div><!-- #listing-author-link	-->
+						</div><!-- #listing-author-description -->
+					</div><!-- #listing-entry-author-info -->
+
+					<?php endif; ?>
 
 					<div class="listing-entry-utility">
-						<?php twentyten_posted_in(); ?>
+						<?php // twentyten_posted_in(); ?>
 						<?php edit_post_link( __( 'Edit', DP_TEXTDOMAIN ), '<span class="listing-edit-link">', '</span>' ); ?>
-					</div><!-- .entry-utility -->
+					</div><!-- .listing-entry-utility -->
+
 				</div><!-- #post-## -->
 
 				<div id="listing-nav-below" class="listing-navigation">
 					<div class="listing-nav-previous"><?php previous_post_link( '%link', '<span class="listing-meta-nav">' . _x( '&larr;', 'Previous post link', DP_TEXTDOMAIN ) . '</span> %title' ); ?></div>
 					<div class="listing-nav-next"><?php next_post_link( '%link', '%title <span class="listing-meta-nav">' . _x( '&rarr;', 'Next post link', DP_TEXTDOMAIN ) . '</span>' ); ?></div>
-				</div><!-- #nav-below -->
+				</div><!-- #listing-nav-below -->
 
 				<?php comments_template( '', true ); ?>
 
-<?php endwhile; // end of the loop. ?>
+			<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #listing-content -->
 		</div><!-- #listing-container -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
-<?php get_header(); ?>
