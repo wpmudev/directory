3ea04ee8e123c7122b170dafde3675a531867daf
ivan <ivan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-05-12 19:16:42 +0000

    Remove old Directory theme files
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/directory@3425 f53fe46e-8c02-459b-9fdd-93e75771b278

--- themes/default-old/home.php
+++
@@ -1,9 +0,0 @@
-<?php get_header() ?>
-<div id="content"><!-- start #content -->
-	<div class="padder">
-		<ul>
-    <?php dp_list_categories('home'); ?>
-</ul>
-	</div>
-</div><!-- end #content -->
-<?php get_footer() ?>
--- themes/default-old/sidebar.php
+++
@@ -1,7 +0,0 @@
-<div id="sidebar"><!-- start #sidebar -->
-	<div class="padder">
-		<?php if ( is_active_sidebar( 'sidebar-default' ) ) : ?>
-			<?php dynamic_sidebar( 'sidebar-default' ); ?>
-		<?php endif; ?>
-	</div>
-</div><!-- end #sidebar -->
